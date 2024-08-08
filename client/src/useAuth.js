ca0bc916166987c726eb66c42799cf3e74dc48b3
Carlynn-Adams <carlynnadams25@gmail.com>
2024-08-08 13:13:35 +0200

    Refresh token automatically

--- client/src/useAuth.js
+++ client/src/useAuth.js
@@ -6,21 +6,40 @@ export default function useAuth(code) {
     const [refreshToken, setRefreshToken] = useState()
     const [expiresIn, setExpiresIn] = useState()
 
-
     useEffect(() => {
-        axios.post('http://localhost:3001/login', {
-            code,
+      axios
+        .post("http://localhost:3001/login", {
+          code,
         })
-        .then(res => {
-            setAccessToken(res.data.accessToken)
-            setRefreshToken(res.data.refreshToken)
-            setExpiresIn(res.data.expiresIn)
-            window.history.pushState({}, null, "/")
+        .then((res) => {
+          setAccessToken(res.data.accessToken);
+          setRefreshToken(res.data.refreshToken);
+          setExpiresIn(res.data.expiresIn);
+          window.history.pushState({}, null, "/");
         })
         .catch(() => {
-            window.location = '/'
-        })
-    }, [code])
+          window.location = "/";
+        });
+    }, [code]);
 
-    return accessToken
+    useEffect(() => {
+      if (!refreshToken || !expiresIn) return;
+      const interval = setInterval(() => {
+        axios
+          .post("http://localhost:3001/refresh", {
+            refreshToken,
+          })
+          .then((res) => {
+            setAccessToken(res.data.accessToken);
+            setExpiresIn(res.data.expiresIn);
+          })
+          .catch(() => {
+            window.location = "/";
+          });
+      }, (expiresIn - 60) * 1000);
+
+      return () => clearInterval(interval);
+    }, [refreshToken, expiresIn]);
+
+    return accessToken;
 }
--- server/server.js
+++ server/server.js
@@ -7,6 +7,29 @@ const app = express();
 app.use(cors())
 app.use(bodyParser.json())
 
+app.post('/refresh', (req, res) => {
+    const refreshToken = req.body.refreshToken
+    const spotifyApi = new SpotifyWebApi({
+      redirectUri: "http:localhost:3000",
+      clientId: "1ec151cbb14c4aefa8e5c1819a26b419",
+      clientSecret: "16f3a27c3c854b74bf4e1e31ae61b79e",
+      refreshToken
+    })
+
+    spotifyApi
+      .refreshAccessToken()
+      .then((data) => {
+        res.json({
+          accessToken: data.body.accessToken,
+          expiresIn: data.body.expiresIn,
+        });
+      })
+      .catch((err) => {
+        console.log(err);
+        res.sendStatus(400);
+      });
+})
+
 app.post('/login', (req, res) => {
     const code = req.body.code
     const spotifyApi = new SpotifyWebApi({
