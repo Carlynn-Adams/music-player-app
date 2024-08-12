d272945ebee8d57555f49f2eccf70ff3d9b9cc40
Carlynn-Adams <carlynnadams25@gmail.com>
2024-08-12 13:01:21 +0200

    Updated URL

--- client/src/Login.js
+++ client/src/Login.js
@@ -2,7 +2,7 @@ import React from 'react'
 import Container from "react-bootstrap/Container";
 
 const AUTH_URL =
-  "https://accounts.spotify.com/authorize?client_id=1ec151cbb14c4aefa8e5c1819a26b419&response_type=code&redirect_uri=http://localhost:3000&scope=streaming%20user-read-email%20user-read-private%20user-library-read%20user-library-modify%20user-read-playback-state%20user-modify-playback-state";
+  "https://melodica-music-player-app.netlify.app/?client_id=1ec151cbb14c4aefa8e5c1819a26b419&response_type=code&redirect_uri=http://localhost:3000&scope=streaming%20user-read-email%20user-read-private%20user-library-read%20user-library-modify%20user-read-playback-state%20user-modify-playback-state";
 
 export default function Login() {
     return (
